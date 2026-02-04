043efee4fb5e9585d77bddbe4cf33dcdfef06a93
Mataisfleis <mateo20102020@gmail.com>
2026-02-03 19:21:18 -0600

    🚧 Add create function

--- src/db.ts
+++ src/db.ts
@@ -16,3 +16,10 @@ export async function getPost(id: number | string){
 
   return request.data
 }
+
+export async function createPost(title: string, content: string){
+  const encodedContent = btoa(content)
+  const request = await axios.post(db_url + "/posts", { title, content: encodedContent, date: Date.now() })
+
+  return request.data
+}
