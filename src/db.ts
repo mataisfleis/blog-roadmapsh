3e4fa415c159e064c72d58428db7476bea4e3731
Mataisfleis <mateo20102020@gmail.com>
2026-02-03 18:59:44 -0600

    🚧 Add get and getAll functions

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { join } from 'node:path';
+
+const db_url = process.env.DB_URL as string
+
+export async function getPosts(){
+  // TODO: Use config instead of using process.env directly
+
+  const request = await axios.get(db_url + '/posts')
+
+  return request.data
+}
+
+export async function getPost(id: number | string){
+  const  request = await axios.get(db_url + `/posts?id=${id}`)
+
+  return request.data
+}
