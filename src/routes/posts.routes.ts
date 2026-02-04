ab415af57a31d7a4f36342d98bdf81528d54a5db
Mataisfleis <mateo20102020@gmail.com>
2026-02-03 20:17:11 -0600

    ⚡ Support markdown content

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { getPost } from '../db.js';
+import showdown from 'showdown'
 import type { Post } from '../types/post.js';
 
 const router: Router = Router()
@@ -8,9 +9,10 @@ router.get('/:id', async (req, res) => {
   const id = req.params.id
   const post: Post = await getPost(id)
   const decodedContent = atob(post.content)
-  console.log(post)
+  const converter = new showdown.Converter()
+  const parsedContent = converter.makeHtml(decodedContent)
   res.render("post", {
-    post, decodedContent
+    post, decodedContent: parsedContent
   })
 })
 
