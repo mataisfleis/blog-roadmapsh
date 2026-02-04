a48648eaa01b351a6a3db6053ec96f1474316461
Mataisfleis <mateo20102020@gmail.com>
2026-02-03 20:10:48 -0600

    ✨  Add  /posts/:id route

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { getPost } from '../db.js';
+import type { Post } from '../types/post.js';
 
 const router: Router = Router()
 
-router.get('/', async (req, res) => {
-  res.send("hello world from posts")
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  const post: Post = await getPost(id)
+  const decodedContent = atob(post.content)
+  console.log(post)
+  res.render("post", {
+    post, decodedContent
+  })
 })
 
 export default router
